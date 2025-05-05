9a744a55ba97de918d1de3fbbc431ddadb8cf3ea
jhokk <jayhokkanen25@gmail.com>
2025-05-05 03:58:21 -0400

    Final

--- main.js
+++ main.js
@@ -13,7 +13,8 @@ let circlePoints = [];
 
 let dt = 0.001;
 let radius = 0.03;
-let mass = 3.0;
+let mass = 5.0;
+
 let hitBox = {
     x: 0.02,
     y: 0.03,
@@ -21,14 +22,17 @@ let hitBox = {
     h: 0.18,
 };
 
-let wobble = false;
-
 let theta = 0;
 let gamma = 0;
+
+let wobble = false;
 let boosting = false;
 let jumping = false;
 let gameOver = false;
+let jumpCooldown = 0;
+let dashCooldown = 0;
 
+// Constant for Catmull-Rom interpolation
 const M = mat4(
     vec4(-0.5, 1.5, -1.5, 0.5),
     vec4(1, -2.5, 2, -0.5),
@@ -36,7 +40,7 @@ const M = mat4(
     vec4(0, 1, 0, 0)
 );
 
-const enemyPts = [
+const enemyPoints = [
     vec2(0.05, 0.0), 
     vec2(-0.05, 0.0), 
     vec2(0.05, 0.05), 
@@ -63,67 +67,37 @@ const weights = [
 ];
 
 // Key presses have the following effects:
-// [a] Do something
+// [a] Dash backwards
+// [d] Dash forwards
+// [SPACE] Jump
 window.addEventListener("keydown", function(event) {
     var key = event.key;
     switch (key) {
-        case 'w':
-            if (!boosting) {
-                velocity = add(velocity, vec2(0, 8));
-                boosting = true;
-            }
-            break;
         case 'a':
-            if (!boosting) {
-                velocity = add(velocity, vec2(-8, 0));
-                boosting = true;
-            }
-            break;
-        case 's':
-            if (!boosting) {
-                velocity = add(velocity, vec2(0, -8));
-                boosting = true;
+            if (dashCooldown <= 0) {
+                velocity = add(velocity, vec2(-11, 2));
+                dashCooldown = 100;
             }
             break;
         case 'd':
-            if (!boosting) {
-                velocity = add(velocity, vec2(8, 0));
-                boosting = true;
+            if (dashCooldown <= 0) {
+                velocity = add(velocity, vec2(11, 2));
+                dashCooldown = 100;
             }
             break;
         case ' ':
-            if (!jumping) {
-                velocity = add(velocity, vec2(0, 8));
+            if (jumpCooldown <= 0) {
+                velocity = add(velocity, vec2(0, 13));
                 position = add(position, vec2(0, 0.05));
-                jumping = true;
+                jumpCooldown = 100;
             }
             break;
     }
 }, false);
 
-window.addEventListener("keyup", function(event) {
-    var key = event.key;
-    switch (key) {
-        case 'w':
-            boosting = false;
-            break;
-        case 'a':
-            boosting = false;
-            break;
-        case 's':
-            boosting = false;
-            break;
-        case 'd':
-            boosting = false;
-            break;
-        case ' ':
-            jumping = false;
-            break;
-    }
-}, false);
 
-// Initialize WebGL and simulation
 function main() {
+    // Initialize WebGL and simulation
     const canvas = document.getElementById("webgl");
     gl = WebGLUtils.setupWebGL(canvas, null);
     if (!gl) { alert("WebGL isn't available"); return; }
@@ -133,38 +107,31 @@ function main() {
     program = initShaders(gl, "vshader", "fshader");
     gl.useProgram(program);
     
+    // Attributes
+    setAttribute("vPosition", vec2(0,0));
+    setAttribute("weights", weights, 4);
+
+    // Uniforms
     gl.uniform1i(gl.getUniformLocation(program, "wobble"), false);
-
-    const vBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(vec2(0, 0)), gl.DYNAMIC_DRAW);
-
-    const vPosition = gl.getAttribLocation(program, "vPosition");
-    gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(vPosition);
-    
-    uploadData("weights", weights, 4);
-
+	gl.uniform4fv(gl.getUniformLocation(program, 'vColor'), flatten([0.0, 0.0, 0.0, 1.0]));
     gl.uniformMatrix4fv(gl.getUniformLocation(program, "modelMatrix"), false, flatten(mat4()));
-
     let cameraMatrix = lookAt( vec3(0, 0, 0), vec3(0, 0, 0), vec3(0, 1, 0) );
     gl.uniformMatrix4fv(gl.getUniformLocation(program, "cameraMatrix"), false, flatten(cameraMatrix));
 
-	gl.uniform4fv(gl.getUniformLocation(program, 'vColor'), flatten([0.0, 0.0, 0.0, 1.0]));
 
     gl.enable(gl.BLEND);
     gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+
+    // Generate circle points
+    for (let i = 0; i < 360; i += 15) {
+        circlePoints.push( vec2(0.025 * Math.cos(radians(i)), 0.025 * Math.sin(radians(i))) );
+    }
 
     
     // Generate initial control points
     for (let i = -1; i < 5; i++) {
         generateStars();
         generateControlPoint();
-    }
-
-    // Generate circle points
-    for (let i = 0; i < 360; i += 15) {
-        circlePoints.push( vec2(0.025 * Math.cos(radians(i)), 0.025 * Math.sin(radians(i))) );
     }
 
     // Generate initial position
@@ -177,10 +144,18 @@ function main() {
 function render() {
     gl.clear(gl.COLOR_BUFFER_BIT);
 
+    // Game loop (do nothing once game is over)
     if (!gameOver) {
+
+        dashCooldown--;
+        jumpCooldown--;
+
+        // Apply physics to player
         updatePlayer();
+
         detectEnemyCollision();
 
+        // If player comes near the end of generated area, generate more
         if (position[0] >= controlPoints.length - 6) {
             generateControlPoint();
             generateStars();
@@ -190,55 +165,60 @@ function render() {
         let cameraMatrix = lookAt( vec3(position[0], position[1], 1), vec3(position[0], position[1], 0), vec3(0, 1, 0) );
         gl.uniformMatrix4fv(gl.getUniformLocation(program, "cameraMatrix"), false, flatten(cameraMatrix));
 
-        gl.uniformMatrix4fv(gl.getUniformLocation(program, "modelMatrix"), false, flatten(mat4()));
-
-        let splinePoints = generateCatmullRomCurve();
-        uploadData("vPosition", splinePoints);
-        gl.drawArrays(gl.LINE_STRIP, 0, splinePoints.length / 2);
-
-        drawPlayer();
-
-        
-        gl.uniformMatrix4fv(gl.getUniformLocation(program, "modelMatrix"), false, flatten(mat4()));
-        uploadData("vPosition", stars);
-        gl.drawArrays(gl.POINTS, 0, stars.length);
-
-
+        // Bone uniforms
+        gamma += 0.10;
         let boneMatrix0 = mat4();
         let boneMatrix1 = mult(rotateZ(5 * Math.sin(gamma)), mat4());
         let boneMatrix2 = mult(rotateZ(5 * Math.sin(gamma)), boneMatrix1);
-        gamma += 0.10;
 
         gl.uniformMatrix4fv(gl.getUniformLocation(program, "boneMatrix0"), false, flatten(boneMatrix0));
         gl.uniformMatrix4fv(gl.getUniformLocation(program, "boneMatrix1"), false, flatten(boneMatrix1));
         gl.uniformMatrix4fv(gl.getUniformLocation(program, "boneMatrix2"), false, flatten(boneMatrix2));
 
+
+        // Draw the spline
+        gl.uniformMatrix4fv(gl.getUniformLocation(program, "modelMatrix"), false, flatten(mat4()));
+        let splinePoints = generateCatmullRomCurve();
+        setAttribute("vPosition", splinePoints);
+        gl.drawArrays(gl.LINE_STRIP, 0, splinePoints.length / 2);
+
+        // Draw the player
+        drawPlayer();
+        
+        // Draw the stars
+        gl.uniformMatrix4fv(gl.getUniformLocation(program, "modelMatrix"), false, flatten(mat4()));
+        setAttribute("vPosition", stars);
+        gl.drawArrays(gl.POINTS, 0, stars.length);
+
+        // Draw the enemies
         drawEnemies();
 
+        // Animate
         requestAnimationFrame(render);
     }
 }
 
-
+// Draws all enemies to the screen
 function drawEnemies() {
-    uploadData("vPosition", enemyPts);
+    setAttribute("vPosition", enemyPoints);
 
     gl.uniform1i(gl.getUniformLocation(program, "wobble"), true);
     for (let e of enemies) {
-        let enemyMat = translate(e[0], e[1], 0.0);
-        gl.uniformMatrix4fv(gl.getUniformLocation(program, "modelMatrix"), false, flatten(enemyMat));
+        let enemyModelMatrix = translate(e[0], e[1], 0.0);
+        gl.uniformMatrix4fv(gl.getUniformLocation(program, "modelMatrix"), false, flatten(enemyModelMatrix));
          
-        gl.drawArrays(gl.TRIANGLE_STRIP, 0, enemyPts.length);
+        gl.drawArrays(gl.TRIANGLE_STRIP, 0, enemyPoints.length);
     }
     gl.uniform1i(gl.getUniformLocation(program, "wobble"), false);
 }
 
-
+// Draws the player to the screen
 function drawPlayer() {
     let stack = [];
     let modelMatrix = mat4();
     let transform = mat4();
-    //theta += 0.1;
+    
+    // Increase theta faster at greater speeds
     theta += Math.max(length(velocity) / 150, 0.05);
 
     let linePoints = [vec2(0, 0), vec2(0, 0.1)];
@@ -250,7 +230,7 @@ function drawPlayer() {
 
         gl.uniformMatrix4fv(gl.getUniformLocation(program, "modelMatrix"), false, flatten(
             mult(modelMatrix, scalem(0.7, 0.7, 1)) ));
-	    uploadData("vPosition", linePoints);
+	    setAttribute("vPosition", linePoints);
 	    gl.drawArrays(gl.LINE_STRIP, 0, linePoints.length);
 
         stack.push(modelMatrix); // Right Upper Leg
@@ -260,7 +240,7 @@ function drawPlayer() {
 
             gl.uniformMatrix4fv(gl.getUniformLocation(program, "modelMatrix"), false, flatten(
                 mult(modelMatrix, scalem(0.5,0.5,1)) ));
-            uploadData("vPosition", linePoints);
+            setAttribute("vPosition", linePoints);
             gl.drawArrays(gl.LINE_STRIP, 0, linePoints.length);
 
             stack.push(modelMatrix); // Right Lower leg
@@ -270,7 +250,7 @@ function drawPlayer() {
 
                 gl.uniformMatrix4fv(gl.getUniformLocation(program, "modelMatrix"), false, flatten(
                     mult(modelMatrix, scalem(0.5,0.5,1)) ));
-                uploadData("vPosition", linePoints);
+                setAttribute("vPosition", linePoints);
                 gl.drawArrays(gl.LINE_STRIP, 0, linePoints.length);
 
                 stack.push(modelMatrix); // Right Foot
@@ -280,7 +260,7 @@ function drawPlayer() {
 
                     gl.uniformMatrix4fv(gl.getUniformLocation(program, "modelMatrix"), false, flatten(
                         mult(modelMatrix, scalem(0.5,0.2,1)) ));
-                    uploadData("vPosition", linePoints);
+                    setAttribute("vPosition", linePoints);
                     gl.drawArrays(gl.LINE_STRIP, 0, linePoints.length);
 
                 modelMatrix = stack.pop();
@@ -295,7 +275,7 @@ function drawPlayer() {
 
             gl.uniformMatrix4fv(gl.getUniformLocation(program, "modelMatrix"), false, flatten(
                 mult(modelMatrix, scalem(0.5,0.5,1)) ));
-            uploadData("vPosition", linePoints);
+            setAttribute("vPosition", linePoints);
             gl.drawArrays(gl.LINE_STRIP, 0, linePoints.length);
 
             stack.push(modelMatrix); // Left Lower leg
@@ -305,7 +285,7 @@ function drawPlayer() {
 
                 gl.uniformMatrix4fv(gl.getUniformLocation(program, "modelMatrix"), false, flatten(
                     mult(modelMatrix, scalem(0.5,0.5,1)) ));
-                uploadData("vPosition", linePoints);
+                setAttribute("vPosition", linePoints);
                 gl.drawArrays(gl.LINE_STRIP, 0, linePoints.length);
 
                 stack.push(modelMatrix); // Left Foot
@@ -315,7 +295,7 @@ function drawPlayer() {
 
                     gl.uniformMatrix4fv(gl.getUniformLocation(program, "modelMatrix"), false, flatten(
                         mult(modelMatrix, scalem(0.5,0.2,1)) ));
-                    uploadData("vPosition", linePoints);
+                    setAttribute("vPosition", linePoints);
                     gl.drawArrays(gl.LINE_STRIP, 0, linePoints.length);
 
                 modelMatrix = stack.pop();
@@ -330,7 +310,7 @@ function drawPlayer() {
 
             gl.uniformMatrix4fv(gl.getUniformLocation(program, "modelMatrix"), false, flatten(
                 mult(modelMatrix, scalem(0.45, 0.45, 1)) ));
-            uploadData("vPosition", linePoints);
+            setAttribute("vPosition", linePoints);
             gl.drawArrays(gl.LINE_STRIP, 0, linePoints.length);
 
             stack.push(modelMatrix); // Right Lower Arm
@@ -340,7 +320,7 @@ function drawPlayer() {
 
                 gl.uniformMatrix4fv(gl.getUniformLocation(program, "modelMatrix"), false, flatten(
                     mult(modelMatrix, scalem(0.35, 0.35, 1)) ));
-                uploadData("vPosition", linePoints);
+                setAttribute("vPosition", linePoints);
                 gl.drawArrays(gl.LINE_STRIP, 0, linePoints.length);
 
             modelMatrix = stack.pop();
@@ -354,7 +334,7 @@ function drawPlayer() {
 
             gl.uniformMatrix4fv(gl.getUniformLocation(program, "modelMatrix"), false, flatten(
                 mult(modelMatrix, scalem(0.45, 0.45, 1)) ));
-            uploadData("vPosition", linePoints);
+            setAttribute("vPosition", linePoints);
             gl.drawArrays(gl.LINE_STRIP, 0, linePoints.length);
 
             stack.push(modelMatrix); // Left Lower Arm
@@ -364,41 +344,30 @@ function drawPlayer() {
 
                 gl.uniformMatrix4fv(gl.getUniformLocation(program, "modelMatrix"), false, flatten(
                     mult(modelMatrix, scalem(0.35, 0.35, 1)) ));
-                uploadData("vPosition", linePoints);
+                setAttribute("vPosition", linePoints);
                 gl.drawArrays(gl.LINE_STRIP, 0, linePoints.length);
 
             modelMatrix = stack.pop();
         modelMatrix = stack.pop();
 
 
-        stack.push(modelMatrix);
+        stack.push(modelMatrix); // Head
 
             transform = translate(0, 0.093, 0);
             modelMatrix = mult(modelMatrix, transform);
 
             gl.uniformMatrix4fv(gl.getUniformLocation(program, "modelMatrix"), false, flatten(modelMatrix));
-            uploadData("vPosition", circlePoints);
+            setAttribute("vPosition", circlePoints);
             gl.drawArrays(gl.LINE_LOOP, 0, circlePoints.length);
 
         modelMatrix = stack.pop();
 
 
     modelMatrix = stack.pop();
-
-    modelMatrix = mat4();
-    
-    let hitBoxPoints = [
-        vec2(position[0] - hitBox.x, position[1] - hitBox.y),
-        vec2(position[0] - hitBox.x+hitBox.w, position[1] - hitBox.y),
-        vec2(position[0] - hitBox.x+hitBox.w, position[1] - hitBox.y+hitBox.h),
-        vec2(position[0] - hitBox.x, position[1] - hitBox.y+hitBox.h)
-    ];
-    gl.uniformMatrix4fv(gl.getUniformLocation(program, "modelMatrix"), false, flatten(modelMatrix));
-    uploadData("vPosition", hitBoxPoints);
-    gl.drawArrays(gl.LINE_LOOP, 0, hitBoxPoints.length);
 }
 
-function uploadData(name, data, size=2) {
+// Sends attribute data to the shader
+function setAttribute(name, data, size=2) {
     let buf = gl.createBuffer();
 	gl.bindBuffer(gl.ARRAY_BUFFER, buf);
 	gl.bufferData(gl.ARRAY_BUFFER, flatten(data), gl.STATIC_DRAW);
@@ -407,6 +376,7 @@ function uploadData(name, data, size=2) {
     gl.enableVertexAttribArray(loc);
 }
 
+// Gets the approximate slope of the spline at a given X
 function getSplineSlope(x) {
     let controlPointIndex = 0;
     while (controlPoints[controlPointIndex + 1][0] <= x) {
@@ -436,12 +406,11 @@ function getSplineSlope(x) {
     return slope;
 }
 
-
+// Generates an array of points for the whole spline
 function generateCatmullRomCurve(segments = 20) {
 	let curve = [];
 	
 	for (var i = 1; i < controlPoints.length-2; i++) {
-		
         for ( var j = 0; j < segments; j++ ) {
 			curve.push( generateCatmullRomPoint(i, j /segments) );
 		}
@@ -450,7 +419,7 @@ function generateCatmullRomCurve(segments = 20) {
 	return new Float32Array( flatten(curve) );
 }
 
-
+// Generates a point on the spline given a control points and T
 function generateCatmullRomPoint(i, T) {
 	let Bx = vec4(controlPoints[i-1][0], controlPoints[i][0], controlPoints[i+1][0], controlPoints[i+2][0]);
 	let By = vec4(controlPoints[i-1][1], controlPoints[i][1], controlPoints[i+1][1], controlPoints[i+2][1]);
@@ -463,7 +432,7 @@ function generateCatmullRomPoint(i, T) {
 	return vec2(x, y);
 }
 
-
+// Gets a point on the spline at a given X
 function getSplinePoint(x) {
     let controlPointIndex = 0;
     while (controlPoints[controlPointIndex + 1][0] <= x) {
@@ -474,14 +443,14 @@ function getSplinePoint(x) {
     return splinePoint;
 }
 
-
+// Adds a new random control point
 function generateControlPoint() {
     let point = vec2(controlPoints[controlPoints.length-1][0]+1, Math.random() * 2.0 - 1);
-    point[1] *= 0.85;
+    point[1] *= 0.7;
     controlPoints.push(point);
 }
 
-
+// Applies physics to the player
 function updatePlayer() {
     let fGravity = vec2(0, -9.81 * mass);
     let fBasic = vec2(2, 0);
@@ -503,6 +472,7 @@ function updatePlayer() {
     position = add(position, scale(dt, velocity));
 }
 
+// Detects if the player collides with the spline and returns a point of collision
 function detectSplineCollision() {
     let checkPoints = [];
 
@@ -520,6 +490,7 @@ function detectSplineCollision() {
     }
 }
 
+// Ends the game if the player collides with an enemy
 function detectEnemyCollision() {
     let nearbyEnemies = [];
     for (let e of enemies) {
@@ -555,6 +526,7 @@ function detectEnemyCollision() {
     }
 }
 
+// Prevents the player from getting below the spline
 function lockPlayerOnSpline() {
     let splinePoint = getSplinePoint(position[0]);
     if (position[1] - radius <= splinePoint[1]) {
@@ -565,13 +537,15 @@ function lockPlayerOnSpline() {
     }
 }
 
+// Generates stars to populate the background
 function generateStars() {
-    for (let i = 0; i < 5; i++) {
-        let star = vec2(controlPoints[controlPoints.length-1][0] + Math.random(), Math.random());
+    for (let i = 0; i < 20; i++) {
+        let star = vec2(controlPoints[controlPoints.length-1][0] + Math.random(), Math.random()*4);
         stars.push(star);
     }
 }
 
+// Randomly generates enemies on the upcoming section of the spline
 function generateEnemies() {
     for (let i = 0; i < 20; i++) {
         if (Math.floor(Math.random() * 30) === 0) {
